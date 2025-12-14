d5bc12f96e0e477f8bb93a411ad929a3d49438ae
Yuktarth Naman <yuktarthnaman@gmail.com>
2025-12-14 19:31:36 +0530

    Serve static frontend file

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,11 @@ const app = express();
 app.use(express.json({ limit: '10mb' }));
 
 // Serve uploaded files
+// Serve uploaded files
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+
+// Serve Frontend
+app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 // Routes
 app.use("/upload", uploadRouter);
